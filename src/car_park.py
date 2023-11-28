7850853fe2335bf788522d3446a6ac4c941a7357
Sue Lim <sue.p.lim@gmail.com>
2023-11-28 21:17:29 +0800

    added constructors and attributes to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,12 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, sensors=None,
+                 displays=None):  # instead of using plate_list, we have parameter names as plates
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):
+        return f"Welcome to car park at {self.location}, with {self.capacity} bays."
+        # def __str__ returns a string containing the car park's location and capacity
