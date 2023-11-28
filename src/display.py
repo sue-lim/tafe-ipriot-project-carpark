198ff073aab8bf3374596eade7244782fa2158a1
Sue Lim <sue.p.lim@gmail.com>
2023-11-28 22:03:12 +0800

    Added constructors and attributes to the display and sesor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,9 @@
 class Display:
-    pass
+    def __init__(self,  id, car_park, message = "", is_on = False ):
+        self.id = id
+        self.message = message
+        self.is_on = is_on or False
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"{self.id}: Display is {'is on' if self.is_on else 'if off'}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,15 @@
 class Sensor:
-    pass
+    """Provide sensors to detect cars"""
+    def __init__(self, id, car_park , is_active = False, ):
+        self.id = id
+        self.car_park = car_park
+        self.is_active = is_active or False
+
+    def __str__(self):
+        return f"{self.id}: Sensor is {'is active' if self.is_active else 'if active'}"
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
+    ...
