3d46c6b18391029f8d1fd66091a3c3e619c52f6a
Sue Lim <sue.p.lim@gmail.com>
2023-11-29 21:50:50 +0800

    Added methods to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,5 +1,7 @@
 from sensor import Sensor
 from display import Display
+
+
 class CarPark:
     def __init__(self, location, capacity, plates=None, sensors=None,
                  displays=None):  # instead of using plate_list, we have parameter names as plates
@@ -9,8 +11,14 @@ class CarPark:
         self.sensors = sensors or []
         self.displays = displays or []
 
+    @property
+    # decorator, which allows a methods to act like an attribute
+    def available_bays(self):
+        # car_park.available_bays
+        return self.capacity - len(self.plates)
+
     def register(self, component):
-        """ Registers component of a carpark"""
+        """ Registers component of a car park"""
         # if  it's not either a Sensor or a Display, throw an error
         if not isinstance(component, (Sensor, Display)):
             raise TypeError("Invalid component type!")
@@ -21,6 +29,18 @@ class CarPark:
         # else, it's a Display, append to Displays
         elif isinstance(component, Display):
             self.displays.append(component)
+
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+
+    def remove_car(self, plate):
+        self.plates.remove(plate)
+
+    def update_displays(self):
+        for display in self.displays:
+            display.update({"Bays": self.available_bays, "Temperature": 42})
+            print(f"Updating: {display}")
 
     def __str__(self):
         return f"Welcome to car park at {self.location}, with {self.capacity} bays."
--- src/display.py
+++ src/display.py
@@ -1,9 +1,13 @@
 class Display:
-    def __init__(self,  id, car_park, message = "", is_on = False ):
+    def __init__(self, id, car_park, message="", is_on=False):
         self.id = id
+        self.car_park = car_park
         self.message = message
         self.is_on = is_on or False
-        self.car_park = car_park
+
+    def update(self, data):
+        for key, value in data.items():
+            print(f"{key}:{value}")
 
     def __str__(self):
         return f"{self.id}: Display is {'is on' if self.is_on else 'if off'}"
